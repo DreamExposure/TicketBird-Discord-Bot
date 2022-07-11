3ee676af4519c7e08d3073e18af61451fc15d62f
NovaFox161 <novamaday@gmail.com>
2022-07-10 19:25:37 -0500

    Fix GlobalCommandRegistrar

--- src/main/kotlin/org/dreamexposure/ticketbird/service/GlobalCommandRegistrar.kt
+++ src/main/kotlin/org/dreamexposure/ticketbird/service/GlobalCommandRegistrar.kt
@@ -1,8 +1,11 @@
 package org.dreamexposure.ticketbird.service
 
+import com.fasterxml.jackson.module.kotlin.readValue
 import discord4j.common.JacksonResources
 import discord4j.discordjson.json.ApplicationCommandRequest
 import discord4j.rest.RestClient
+import org.dreamexposure.ticketbird.logger.LOGGER
+import org.dreamexposure.ticketbird.utils.GlobalVars.DEFAULT
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
@@ -22,10 +25,13 @@ class GlobalCommandRegistrar(
 
         val commands = mutableListOf<ApplicationCommandRequest>()
         for (res in matcher.getResources("commands/*.json")) {
-            val request = d4jMapper.objectMapper.readValue(res.toString(), ApplicationCommandRequest::class.java)
+            val request = d4jMapper.objectMapper.readValue<ApplicationCommandRequest>(res.inputStream)
             commands.add(request)
         }
 
-        applicationService.bulkOverwriteGlobalApplicationCommand(applicationId, commands).subscribe()
+        applicationService.bulkOverwriteGlobalApplicationCommand(applicationId, commands)
+            .doOnNext { LOGGER.debug("Bulk overwrite read: ${it.name()}") }
+            .doOnError { LOGGER.error(DEFAULT, "Bulk overwrite failed", it) }
+            .subscribe()
     }
 }
