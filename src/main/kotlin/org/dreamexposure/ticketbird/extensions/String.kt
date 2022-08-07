c8c603046a66ccc0de40249124854d481199c81f
NovaFox161 <novamaday@gmail.com>
2022-08-07 11:29:22 -0500

    Webhook logs should no longer error on content length

--- src/main/kotlin/org/dreamexposure/ticketbird/extensions/String.kt
+++ src/main/kotlin/org/dreamexposure/ticketbird/extensions/String.kt
@@ -11,3 +11,11 @@ fun String.handleLocaleDebt(): Locale {
         else -> Locale.forLanguageTag(this)
     }
 }
+
+fun String.embedTitleSafe(): String = this.substring(0, (256).coerceAtMost(this.length))
+
+fun String.embedDescriptionSafe(): String = this.substring(0, (4096).coerceAtMost(this.length))
+
+fun String.embedFieldSafe(): String = this.substring(0, (1024).coerceAtMost(this.length))
+
+fun String.messageContentSafe(): String = this.substring(0, (2000).coerceAtMost(this.length))
--- src/main/kotlin/org/dreamexposure/ticketbird/logger/DiscordLogAppender.kt
+++ src/main/kotlin/org/dreamexposure/ticketbird/logger/DiscordLogAppender.kt
@@ -8,6 +8,7 @@ import club.minnced.discord.webhook.send.WebhookEmbedBuilder
 import org.dreamexposure.ticketbird.GitProperty
 import org.dreamexposure.ticketbird.TicketBird
 import org.dreamexposure.ticketbird.config.BotSettings
+import org.dreamexposure.ticketbird.extensions.embedFieldSafe
 import org.dreamexposure.ticketbird.utils.GlobalVars
 import org.dreamexposure.ticketbird.utils.GlobalVars.DEFAULT
 import org.dreamexposure.ticketbird.utils.GlobalVars.STATUS
@@ -56,16 +57,16 @@ class DiscordWebhookAppender : AppenderBase<ILoggingEvent>() {
             .setTitle(WebhookEmbed.EmbedTitle("Status", null))
             .addField(WebhookEmbed.EmbedField(true, "Shard Index", "${TicketBird.getShardIndex()}"))
             .addField(WebhookEmbed.EmbedField(true, "Time", "<t:${event.timeStamp / 1000}:f>"))
-            .addField(WebhookEmbed.EmbedField(false, "Logger", event.loggerName))
+            .addField(WebhookEmbed.EmbedField(false, "Logger", event.loggerName.embedFieldSafe()))
             .addField(WebhookEmbed.EmbedField(true, "Level", event.level.levelStr))
-            .addField(WebhookEmbed.EmbedField(true, "Thread", event.threadName))
-            .setDescription(event.formattedMessage)
+            .addField(WebhookEmbed.EmbedField(true, "Thread", event.threadName.embedFieldSafe()))
+            .setDescription(event.formattedMessage.embedFieldSafe())
             .setColor(GlobalVars.embedColor.rgb)
             .setFooter(WebhookEmbed.EmbedFooter("v${GitProperty.TICKETBIRD_VERSION.value}", null))
             .setTimestamp(Instant.now())
 
         if (event.throwableProxy != null) {
-            content.addField(WebhookEmbed.EmbedField(false, "Error Message", event.throwableProxy.message))
+            content.addField(WebhookEmbed.EmbedField(false, "Error Message", event.throwableProxy.message.embedFieldSafe()))
             content.addField(
                 WebhookEmbed.EmbedField(
                     false,
@@ -83,16 +84,16 @@ class DiscordWebhookAppender : AppenderBase<ILoggingEvent>() {
             .setTitle(WebhookEmbed.EmbedTitle(event.level.levelStr, null))
             .addField(WebhookEmbed.EmbedField(true, "Shard Index", "${TicketBird.getShardIndex()}"))
             .addField(WebhookEmbed.EmbedField(true, "Time", "<t:${event.timeStamp / 1000}:f>"))
-            .addField(WebhookEmbed.EmbedField(false, "Logger", event.loggerName))
+            .addField(WebhookEmbed.EmbedField(false, "Logger", event.loggerName.embedFieldSafe()))
             .addField(WebhookEmbed.EmbedField(true, "Level", event.level.levelStr))
-            .addField(WebhookEmbed.EmbedField(true, "Thread", event.threadName))
-            .setDescription(event.formattedMessage)
+            .addField(WebhookEmbed.EmbedField(true, "Thread", event.threadName.embedFieldSafe()))
+            .setDescription(event.formattedMessage.embedFieldSafe())
             .setColor(GlobalVars.embedColor.rgb)
             .setFooter(WebhookEmbed.EmbedFooter("v${GitProperty.TICKETBIRD_VERSION.value}", null))
             .setTimestamp(Instant.now())
 
         if (event.throwableProxy != null) {
-            content.addField(WebhookEmbed.EmbedField(false, "Error Message", event.throwableProxy.message))
+            content.addField(WebhookEmbed.EmbedField(false, "Error Message", event.throwableProxy.message.embedFieldSafe()))
             content.addField(
                 WebhookEmbed.EmbedField(
                     false,
