2b69f4a551923b70a141465748978685bb5de824
NovaFox161 <novamaday@gmail.com>
2022-11-24 23:10:47 -0600

    Move discord beans into its own class

--- src/main/kotlin/org/dreamexposure/ticketbird/config/WebFluxConfig.kt
+++ src/main/kotlin/org/dreamexposure/ticketbird/config/WebFluxConfig.kt
@@ -1,36 +1,5 @@
 package org.dreamexposure.ticketbird.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import discord4j.common.JacksonResources
-import discord4j.common.store.Store
-import discord4j.common.store.legacy.LegacyStoreLayout
-import discord4j.core.DiscordClientBuilder
-import discord4j.core.GatewayDiscordClient
-import discord4j.core.event.domain.Event
-import discord4j.core.`object`.presence.ClientActivity
-import discord4j.core.`object`.presence.ClientPresence
-import discord4j.core.shard.MemberRequestFilter
-import discord4j.core.shard.ShardingStrategy
-import discord4j.discordjson.json.GuildData
-import discord4j.discordjson.json.MessageData
-import discord4j.gateway.intent.Intent
-import discord4j.gateway.intent.IntentSet
-import discord4j.rest.RestClient
-import discord4j.store.api.mapping.MappingStoreService
-import discord4j.store.api.service.StoreService
-import discord4j.store.jdk.JdkStoreService
-import discord4j.store.redis.RedisClusterStoreService
-import discord4j.store.redis.RedisStoreService
-import io.lettuce.core.RedisClient
-import io.lettuce.core.RedisURI
-import io.lettuce.core.cluster.RedisClusterClient
-import kotlinx.coroutines.reactor.mono
-import org.dreamexposure.ticketbird.TicketBird
-import org.dreamexposure.ticketbird.listeners.EventListener
-import org.dreamexposure.ticketbird.mapper.SnowflakeMapper
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.server.ConfigurableWebServerFactory
 import org.springframework.boot.web.server.ErrorPage
 import org.springframework.boot.web.server.WebServerFactoryCustomizer
@@ -38,7 +7,6 @@ import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.config.CorsRegistry
@@ -53,7 +21,6 @@ import org.thymeleaf.spring5.SpringWebFluxTemplateEngine
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver
 import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver
 import org.thymeleaf.templatemode.TemplateMode
-import reactor.kotlin.core.publisher.toFlux
 
 
 @Configuration
@@ -116,89 +83,4 @@ class WebFluxConfig : WebServerFactoryCustomizer<ConfigurableWebServerFactory>, 
 
         return viewResolver
     }
-
-    // Discord
-    @Bean
-    @Primary
-    fun objectMapper(): ObjectMapper {
-        // Use d4j's object mapper
-        return JacksonResources.create().objectMapper
-            .registerKotlinModule()
-            .registerModule(JavaTimeModule())
-            .registerModule(SnowflakeMapper())
-    }
-
-    @Bean
-    fun discordGatewayClient(
-        @Value("\${bot.secret.token}") token: String,
-        listeners: List<EventListener<*>>,
-        stores: StoreService
-    ): GatewayDiscordClient {
-        return DiscordClientBuilder.create(token)
-            .build().gateway()
-            .setEnabledIntents(getIntents())
-            .setSharding(getStrategy())
-            .setStore(Store.fromLayout(LegacyStoreLayout.of(stores)))
-            .setInitialPresence { ClientPresence.doNotDisturb(ClientActivity.playing("Booting Up!")) }
-            .setMemberRequestFilter(MemberRequestFilter.none())
-            .withEventDispatcher { dispatcher ->
-                @Suppress("UNCHECKED_CAST")
-                (listeners as Iterable<EventListener<Event>>).toFlux()
-                    .flatMap {
-                        dispatcher.on(it.genericType) { event -> mono { it.handle(event) } }
-                    }
-            }
-            .login()
-            .block()!!
-    }
-
-    @Bean
-    fun discordRestClient(gatewayDiscordClient: GatewayDiscordClient): RestClient {
-        return gatewayDiscordClient.restClient
-    }
-
-    private fun getStrategy(): ShardingStrategy {
-        return ShardingStrategy.builder()
-            .count(TicketBird.getShardCount())
-            .indices(TicketBird.getShardIndex())
-            .build()
-    }
-
-    @Bean
-    fun discordStores(
-        @Value("\${bot.cache.redis:false}") useRedis: Boolean,
-        @Value("\${spring.redis.host:null}") redisHost: String?,
-        @Value("\${spring.redis.port:null}") redisPort: String?,
-        @Value("\${spring.redis.password:null}") redisPassword: CharSequence?,
-        @Value("\${redis.cluster:false}") redisCluster: Boolean,
-    ): StoreService {
-        return if (useRedis) {
-            val uriBuilder = RedisURI.Builder
-                .redis(redisHost, redisPort!!.toInt())
-            if (redisPassword != null) uriBuilder.withPassword(redisPassword)
-
-            val rss = if (redisCluster) {
-                RedisClusterStoreService.Builder()
-                    .redisClient(RedisClusterClient.create(uriBuilder.build()))
-                    .build()
-            } else {
-                RedisStoreService.Builder()
-                    .redisClient(RedisClient.create(uriBuilder.build()))
-                    .build()
-            }
-
-
-            MappingStoreService.create()
-                .setMappings(rss, GuildData::class.java, MessageData::class.java)
-                .setFallback(JdkStoreService())
-        } else JdkStoreService()
-    }
-
-    private fun getIntents() = IntentSet.of(
-        Intent.GUILDS,
-        Intent.GUILD_MESSAGES,
-        Intent.GUILD_MESSAGE_REACTIONS,
-        Intent.DIRECT_MESSAGES,
-        Intent.DIRECT_MESSAGE_REACTIONS
-    )
 }
