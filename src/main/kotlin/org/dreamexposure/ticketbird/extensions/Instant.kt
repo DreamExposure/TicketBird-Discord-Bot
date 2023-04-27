38731318756ec59178914a6c0d6da87a532569c4
NovaFox161 <novamaday@gmail.com>
2023-04-27 17:18:50 -0500

    Fix error when generating timestamp

--- src/main/kotlin/org/dreamexposure/ticketbird/extensions/Instant.kt
+++ src/main/kotlin/org/dreamexposure/ticketbird/extensions/Instant.kt
@@ -1,8 +1,9 @@
 package org.dreamexposure.ticketbird.extensions
 
 import java.time.Instant
+import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 fun Instant.ticketLogFileFormat(): String {
-    return DateTimeFormatter.ofPattern("yyyy LLL dd HH:mm:ss z").format(this)
+    return DateTimeFormatter.ofPattern("yyyy LLL dd HH:mm:ss z").withZone(ZoneOffset.UTC).format(this)
 }
