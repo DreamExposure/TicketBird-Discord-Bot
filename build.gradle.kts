72476c37ef9a28469bddc68ca7d6dfd4b7eead7e
NovaFox161 <nova@maday.dev>
2025-06-16 18:03:14 -0500

    Upgrade Kotlin to v2.1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     // Kotlin
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.1.21"
 
     // Spring
-    kotlin("plugin.spring") version "2.0.20"
+    kotlin("plugin.spring") version "2.1.21"
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
 
