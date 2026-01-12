aaa1153b580640a0f19a6886897c236bf8783566
NovaFox161 <nova@maday.dev>
2026-01-12 03:13:10 -0600

    I mean, it definitely works for getting migrations working

--- build.gradle.kts
+++ build.gradle.kts
@@ -184,7 +184,7 @@ tasks {
         dependsOn(generateGitProperties)
 
         compilerOptions {
-            freeCompilerArgs.set(listOf("-Xjsr305=strict"))
+            freeCompilerArgs.set(listOf("-Xjsr305=strict", "-Xannotation-default-target=param-property"))
             jvmTarget.set(JvmTarget.fromTarget(java.targetCompatibility.majorVersion))
         }
     }
